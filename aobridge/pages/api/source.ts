43f76076b7eabbab72f0d72341727e64e73c46e5
smallfu6 <smallfu666@gmail.com>
2024-10-31 19:05:10 +0800

    fix: config source

--- aobridge/pages/api/source.ts
+++ aobridge/pages/api/source.ts
@@ -2,7 +2,7 @@ export const Source = `
 const url = 'https://cu.ao-testnet.xyz/dry-run?process-id=OKo3TSifCu4Q_4_fUo7XrAQq9_WaDuYzKaxoEFY4VBw';
 
 // proof verify
-const proof = [proof]
+const proof = "[proof]"
 const data = {"Id":"1234","Target":"OKo3TSifCu4Q_4_fUo7XrAQq9_WaDuYzKaxoEFY4VBw","Owner":"1234","Anchor":"0","Data": proof,"Tags":[{"name":"Action","value":"GetVerifyResult"},{"name":"Data-Protocol","value":"ao"},{"name":"Type","value":"Message"},{"name":"Variant","value":"ao.TN.1"}]}
 
 const apiResponseTrans = await Functions.makeHttpRequest({
